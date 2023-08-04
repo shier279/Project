21d2f8fffe4514fb99915500a792ea44933640d1
shier279 <129755921+shier279@users.noreply.github.com>
2023-08-04 22:18:42 +0800

    Update project14.py

--- project14.py
+++ project14.py
@@ -1,38 +1,43 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric import rsa
+from gmssl.sm2 import CryptSM2, SM2PrivateKey, SM2PublicKey
 import time
 
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
-)
-public_key = private_key.public_key()
+def generate_key_pair():
+    private_key = SM2PrivateKey()
+    private_key.generate()
+    public_key = private_key.public_key
+    return private_key, public_key
 
-message = b"Hello, World!"
-encrypted_message = public_key.encrypt(
-    message,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+def encrypt(message, public_key):
+    cipher = CryptSM2(public_key=public_key)
+    encrypted_data = cipher.encrypt(message.encode())
+    return encrypted_data
 
-decrypted_message = private_key.decrypt(
-    encrypted_message,
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+def decrypt(encrypted_data, private_key):
+    cipher = CryptSM2(private_key=private_key)
+    decrypted_data = cipher.decrypt(encrypted_data)
+    return decrypted_data.decode()
+
+def sign(message, private_key):
+    signer = CryptSM2(private_key=private_key)
+    signature = signer.sign(message.encode())
+    return signature
+
+def verify(message, signature, public_key):
+    verifier = CryptSM2(public_key=public_key)
+    is_valid = verifier.verify(message.encode(), signature)
+    return is_valid
 
 start = time.perf_counter()
-print("Original message:", message)
-print("Encrypted message:", encrypted_message)
-print("Decrypted message:", decrypted_message)
+message = "Hello, World!"
+private_key, public_key = generate_key_pair()
+encrypted_data = encrypt(message, public_key)
+decrypted_data = decrypt(encrypted_data, private_key)
+signature = sign(message, private_key)
+is_valid = verify(message, signature, public_key)
+
+print("加密后的数据：", encrypted_data)
+print("解密后的数据：", decrypted_data)
+print("数字签名：", signature)
+print("签名验证结果：", is_valid)
 end = time.perf_counter()
 print(end - start)
